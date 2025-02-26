73bf83c620a9f47340744a9a5859d08302ff3e54
TatyanaDev <kot.kate2020@gmail.com>
2025-02-26 13:54:32 +0100

    Add update a technology for projects

--- backend/src/collections/Technologies.ts
+++ backend/src/collections/Technologies.ts
@@ -13,4 +13,36 @@ export const Technologies: CollectionConfig = {
     },
   ],
   upload: true,
+  hooks: {
+    afterChange: [
+      async ({ doc, req }) => {
+        try {
+          const projects = await req.payload.find({
+            collection: 'projects',
+            where: {
+              technologies: {
+                in: [doc.id],
+              },
+            },
+          })
+
+          await Promise.all(
+            projects.docs.map((project) =>
+              req.payload.update({
+                collection: 'projects',
+                id: project.id,
+                data: {
+                  technologies: project.technologies.map((technology) =>
+                    technology === doc.id ? doc : technology,
+                  ),
+                },
+              }),
+            ),
+          )
+        } catch (error) {
+          console.error('Error while updating projects with new technology:', error)
+        }
+      },
+    ],
+  },
 }
