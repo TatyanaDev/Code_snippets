44f2eb3da82122e4513cccd4699cc6b2b87b73b8
TatyanaDev <kot.kate2020@gmail.com>
2025-02-26 11:24:45 +0100

    add takeScreenshot and puppeteer

---
+++ backend/src/utils/takeScreenshot.ts
@@ -0,0 +1,15 @@
+import puppeteer from 'puppeteer'
+
+export default async function takeScreenshot(url: string): Promise<Buffer> {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await page.setViewport({ width: 1920, height: 1080 })
+  await page.goto(url, { waitUntil: 'networkidle2' })
+
+  const screenshotBuffer = await page.screenshot({ type: 'png' })
+
+  await browser.close()
+
+  return Buffer.from(screenshotBuffer)
+}
