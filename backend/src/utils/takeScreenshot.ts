a1abd9b8992a4115ffbdb2a7bc1c72599ff08ec3
TatyanaDev <kot.kate2020@gmail.com>
2025-03-29 14:42:11 +0100

    ass settings for puppeteer

--- backend/src/utils/takeScreenshot.ts
+++ backend/src/utils/takeScreenshot.ts
@@ -2,7 +2,15 @@ import puppeteer from 'puppeteer'
 
 export default async function takeScreenshot(url: string): Promise<Buffer> {
   const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--headless'],
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-gpu',
+      '--headless',
+      '--disable-software-rasterizer',
+    ],
+    dumpio: true,
+    timeout: 0,
   })
   const page = await browser.newPage()
 
