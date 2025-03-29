f4da31dadadc4db41cbc1acf6d486a015de850e4
TatyanaDev <kot.kate2020@gmail.com>
2025-03-29 14:20:07 +0100

    add settings for puppeteer

--- backend/src/utils/takeScreenshot.ts
+++ backend/src/utils/takeScreenshot.ts
@@ -1,11 +1,13 @@
 import puppeteer from 'puppeteer'
 
 export default async function takeScreenshot(url: string): Promise<Buffer> {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--headless'],
+  })
   const page = await browser.newPage()
 
   await page.setViewport({ width: 1920, height: 1080 })
-  await page.goto(url, { waitUntil: 'networkidle0' })
+  await page.goto(url, { waitUntil: 'networkidle0', timeout: 0 })
 
   const screenshotBuffer = await page.screenshot({ type: 'png' })
 
