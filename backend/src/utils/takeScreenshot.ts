a35243e7108d2eaa5c1569bd57ad26b0a71e8b10
TatyanaDev <kot.kate2020@gmail.com>
2025-04-01 12:33:40 +0200

    fix puppeteer

--- backend/src/utils/takeScreenshot.ts
+++ backend/src/utils/takeScreenshot.ts
@@ -1,26 +1,43 @@
-import puppeteer from 'puppeteer'
+import puppeteer, { Browser } from 'puppeteer'
 
 export default async function takeScreenshot(url: string): Promise<Buffer> {
-  const browser = await puppeteer.launch({
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-gpu',
-      '--headless',
-      '--disable-software-rasterizer',
-    ],
-    headless: true,
-    dumpio: true,
-    timeout: 0,
-  })
-  const page = await browser.newPage()
+  let browser: Browser | null = null
 
-  await page.setViewport({ width: 1920, height: 1080 })
-  await page.goto(url, { waitUntil: 'networkidle0', timeout: 0 })
+  try {
+    browser = await puppeteer.launch({
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-gpu',
+        '--headless',
+        '--disable-software-rasterizer',
+        '--remote-debugging-port=9222',
+      ],
+      headless: true,
+      dumpio: true,
+      timeout: 0,
+    })
 
-  const screenshotBuffer = await page.screenshot({ type: 'png' })
+    const page = await browser.newPage()
 
-  await browser.close()
+    await page.setViewport({ width: 1920, height: 1080 })
 
-  return Buffer.from(screenshotBuffer)
+    await page.goto(url, { waitUntil: 'networkidle0', timeout: 0 })
+
+    const screenshotBuffer: Uint8Array = await page.screenshot({ type: 'png' })
+
+    const buffer = Buffer.from(screenshotBuffer)
+
+    return buffer
+  } catch (error) {
+    console.error('Error while taking screenshot:', error)
+    if (browser) {
+      await browser.close()
+    }
+    throw error
+  } finally {
+    if (browser) {
+      await browser.close()
+    }
+  }
 }
