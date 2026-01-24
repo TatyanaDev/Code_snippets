cd71d610d3bc127d7289a4fd7e9c061b58e4d1eb
TatyanaDev <kot.kate2020@gmail.com>
2026-01-24 13:39:59 +0100

    update versions of libraries

--- backend/next.config.mjs
+++ backend/next.config.mjs
@@ -1,9 +1,14 @@
 import { withPayload } from '@payloadcms/next/withPayload'
+import { fileURLToPath } from 'url'
+import path from 'path'
 
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  outputFileTracingRoot: path.join(path.dirname(fileURLToPath(import.meta.url))),
   basePath: process.env.NODE_ENV === 'production' ? '/dev-sketches' : '',
+  reactStrictMode: true,
+  devIndicators: false,
   trailingSlash: true,
 }
 
